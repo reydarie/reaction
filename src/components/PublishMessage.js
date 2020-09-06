41ac6f6f289cbdc38124292d1d53b6f608a48fd0
Rey <dario-edgardo.rey@capgemini.com>
2020-09-06 18:29:03 -0300

    Created PublishMessage Component

---
+++ src/components/PublishMessage.js
@@ -0,0 +1,32 @@
+import React, { useState, useReducer } from "react";
+import { newMessage } from "../state/actions";
+
+function PublishMessage({ dispatch }) {
+  const [text, setText] = useState("");
+
+  const updateText = (event) => {
+    setText(event.target.value);
+  };
+
+  const publishMessage = () => {
+    dispatch(newMessage(text));
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") publishMessage();
+  };
+
+  return (
+    <div>
+      <h3>Got Something to say?</h3>
+      <input
+        value={text}
+        onChange={updateText}
+        onKeyPress={handleKeyPress}
+      />{" "}
+      <button onClick={publishMessage}>Publish it!</button>
+    </div>
+  );
+}
+
+export default PublishMessage;
