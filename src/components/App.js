331b4681298e97b710f7e6a7b12d5d3e7d695671
Rey <dario-edgardo.rey@capgemini.com>
2020-09-13 20:05:15 -0300

    added setusername component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,38 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import { reducer, initialState } from "../state/reducer";
 import PublishMessage from "./PublishMessage";
 import MessageBoard from "./MessageBoard";
 import Context from "../context";
+import PubSub from "../pubsub";
+import SetUsername from "./SetUsername";
+
+const pubsub = new PubSub();
+
+setTimeout(() => {
+  pubsub.publish({ type: "foo", value: "bar" });
+}, 1000);
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  useEffect(() => {
+    pubsub.addListener({
+      message: (messageObject) => {
+        const { channel, message } = messageObject;
+
+        console.log("Received message", message, "channel", channel);
+        dispatch(message);
+      },
+    });
+  }, []);
+
   console.log("state", state);
 
   return (
-    <Context.Provider value={{state, dispatch}}>
+    <Context.Provider value={{ state, dispatch, pubsub }}>
       <div>
         <h2>Reaction</h2>
+        <SetUsername />
         <hr />
         <PublishMessage />
         <hr />
--- src/state/actions.js
+++ src/state/actions.js
@@ -1,7 +1,9 @@
-import { NEW_MESSAGE } from "./types";
+import { NEW_MESSAGE, SET_USERNAME } from "./types";
 import uuid from "uuid/v4";
 
 export const newMessage = (text) => ({
   type: NEW_MESSAGE,
   item: { id: uuid(), text, timestamp: Date.now() },
 });
+
+export const setUsername = (username) => ({ type: SET_USERNAME, username });
